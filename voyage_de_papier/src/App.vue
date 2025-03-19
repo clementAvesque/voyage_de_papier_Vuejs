480ca1304919f294b5b5d0d8c4375a68f1160a53
clementAvesque <clement2avesque@gmail.com>
2025-03-19 10:15:26 +0100

    rajout du router

--- voyage_de_papier/src/App.vue
+++ voyage_de_papier/src/App.vue
@@ -1,30 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-    <header>
-        <img src="" alt="">
-        <div class="header_list">
-            <ul>
-                <li>Acceuil</li>
-                <li>Votre journal</li>
-            </ul>
-        </div>
-        <p id="name">Voyage de papier</p>
-        <div class="header_list">
-            <ul>
-                <li>Notre équipe</li>
-                <li>FAQ</li>
-            </ul>
-            
-        </div>
-        <img src="img/achat.svg" alt="">
-    </header>
+  <div>
+    <nav>
 
-  <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
