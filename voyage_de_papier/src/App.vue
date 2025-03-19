0c2927bd478a99d2c129fb692e6d5aa99320ece5
Ninon Roche <rocheninon74@gmail.com>
2025-03-19 10:15:18 +0100

    ajout header

--- voyage_de_papier/src/App.vue
+++ voyage_de_papier/src/App.vue
@@ -1,30 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue'; 
 </script>
 
 <template>
-    <header>
-        <img src="" alt="">
-        <div class="header_list">
-            <ul>
-                <li>Acceuil</li>
-                <li>Votre journal</li>
-            </ul>
-        </div>
-        <p id="name">Voyage de papier</p>
-        <div class="header_list">
-            <ul>
-                <li>Notre équipe</li>
-                <li>FAQ</li>
-            </ul>
-            
-        </div>
-        <img src="img/achat.svg" alt="">
-    </header>
-
-  <HelloWorld msg="Vite + Vue" />
+  <Header />
 </template>
 
 <style scoped>
-
 </style>
---
+++ voyage_de_papier/src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="bg-[#3C0D11] py-4 flex justify-center items-center gap-10 text-white text-lg">
+      <a href="#" class="hover:underline">Accueil</a>
+      <a href="#" class="hover:underline">Votre journal</a>
+      <a href="#" class="hover:underline">Notre équipe</a>
+      <a href="#" class="hover:underline">FAQ</a>
+      <button class="ml-10">
+        <img src="" alt="Panier" class="panier" />
+      </button>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "header",
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
